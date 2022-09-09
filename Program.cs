87d69687f393e898a31c7a267bc5624cea27edb6
Aaron Ullom <uaaron@vmware.com>
2022-09-09 14:51:33 -0500

    Add CORS policy

--- Program.cs
+++ Program.cs
@@ -1,11 +1,22 @@
 var builder = WebApplication.CreateBuilder(args);
 
+var MyCorsPolicy = "_myCorsPolicy";
+
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyCorsPolicy,
+    policy =>
+    {
+        policy.WithOrigins("http://localhost:3000");
+    });
+});
 
 var app = builder.Build();
 
@@ -23,3 +34,5 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+app.UseCors(MyCorsPolicy);
